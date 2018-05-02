68ecda13a416a198e5141cc3c3652a17f303609f
victor hugo monteiro de oliveira <victormonteiro@id.uff.br>
2018-05-02 16:25:29 -0300

    add project

---
+++ VRProject/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    //MeshRenderer obj;
+    Material obj;
+    Color baseColor;
+	// Use this for initialization
+	void Start () {
+        obj = this.GetComponent<Renderer>().material;
+        baseColor = obj.color;
+	}
+
+    public void changeColor() {
+        obj.color = Color.white;
+    }
+
+    public void resetColor()
+    {
+        obj.color = baseColor;
+    }
+}
